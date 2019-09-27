915b039527b8a778dc8234e7571866479573ebb8
Rayan <yweiyun@163.com>
2019-09-27 12:20:11 +0800

    init

--- cista/test/hash_test.cc
+++
@@ -1,9 +0,0 @@
-#include "doctest.h"
-
-#ifdef SINGLE_HEADER
-#include "cista.h"
-#else
-#include "cista/type_hash/type_name.h"
-#endif
-
-TEST_CASE("type_hash<int>") { CHECK(cista::type_str<int>() == "int"); }
